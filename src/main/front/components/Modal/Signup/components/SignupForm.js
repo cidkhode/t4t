7904b015fa4ad7b32ebf5d3692fe51cbea0871a7
Bernin Alejandro Uben <berninuben@icloud.com>
2019-09-30 17:49:06 -0400

    [storyID: 103] added sliding animation between panels

--- src/main/front/components/Modal/Signup/components/SignupForm.js
+++ src/main/front/components/Modal/Signup/components/SignupForm.js
@@ -35,9 +35,7 @@ class SignupForm extends React.Component {
 	}
 
 	setStep(step) {
-		if (this.state.stepCtr === this.state.lastStep) {
-			return;
-		} else if (0 < step && step <= this.state.maxStep) {
+		if (0 < step && step <= this.state.maxStep && !(this.state.stepCtr === this.state.lastStep)) {
 			this.setState({ stepCtr: step });
 		}
 	}
@@ -81,57 +79,57 @@ class SignupForm extends React.Component {
 				}}
 			>
 				{({ isSubmitting }) => (
-				<Form>
-					<div id="steps">
-						<div id="step-1" className={`step ${this.state.stepCtr === 1 ? "active" : ""}`}>
-							<Field type="text" name="usertype" placeholder="Reader or Writer" />
-							<ErrorMessage name="usertype" component="div" />
+					<Form>
+						<div id="steps" className={`curr-step-${this.state.stepCtr}`}>
+							<div id="step-1" className={`step ${this.state.stepCtr >= 1 ? "active" : ""} ${this.state.stepCtr >= 2 ? "active-hide" : ""}`}>
+								<Field type="text" name="usertype" placeholder="Reader or Writer" />
+								<ErrorMessage name="usertype" component="div" />
 
-							<Field type="text" name="name" placeholder="Name" />
-							<ErrorMessage name="name" component="div" />
+								<Field type="text" name="name" placeholder="Name" />
+								<ErrorMessage name="name" component="div" />
 
-							<Field type="email" name="email" placeholder="Email" />
-							<ErrorMessage name="email" component="div" />
+								<Field type="email" name="email" placeholder="Email" />
+								<ErrorMessage name="email" component="div" />
 
-							<Field type="password" name="password" placeholder="Password" />
-							<ErrorMessage name="password" component="div" />
+								<Field type="password" name="password" placeholder="Password" />
+								<ErrorMessage name="password" component="div" />
 
-							<Field type="password" name="confirmpass" placeholder="Confirm Password" />
-							<ErrorMessage name="confirm-pass" component="div" />
+								<Field type="password" name="confirmpass" placeholder="Confirm Password" />
+								<ErrorMessage name="confirm-pass" component="div" />
+							</div>
+
+							<div id="step-2" className={`step ${this.state.stepCtr >= 2 ? "active" : ""} ${this.state.stepCtr >= 2 ? "active-hide" : ""}`}>
+								<Field type="text" name="interests" placeholder="Interests" />
+								<ErrorMessage name="interests" component="div" />
+
+								<Field type="text" name="field_study" placeholder="Field of Study" />
+								<ErrorMessage name="field_study" component="div" />
+
+								<Field type="text" name="view_points" placeholder="View Points" />
+								<ErrorMessage name="view_points" component="div" />
+							</div>
+
+							<div id="step-3" className={`step ${this.state.stepCtr === 3 ? "active" : ""} ${this.state.stepCtr >= 2 ? "active-hide" : ""}`}>
+								<p> Registration Complete. </p>
+								<p> Please confirm your email. </p>
+							</div>
 						</div>
 
-						<div id="step-2" className={`step ${this.state.stepCtr === 2 ? "active" : ""}`}>
-							<Field type="text" name="interests" placeholder="Interests" />
-							<ErrorMessage name="interests" component="div" />
 
-							<Field type="text" name="field_study" placeholder="Field of Study" />
-							<ErrorMessage name="field_study" component="div" />
+						<div id="signup-controls">
+							<div id="step-nav">
+								<div onClick={this.getPrevStep} className={`arrow-left ${this.state.stepCtr === 2 ? "active" : ""}`}></div>
+								{this.getCurrStep(this.state.stepCtr)}
+								<div onClick={this.getNextStep} className={`arrow-right ${this.state.stepCtr < 3 ? "active" : ""}`}></div>
+							</div>
 
-							<Field type="text" name="view_points" placeholder="View Points" />
-							<ErrorMessage name="view_points" component="div" />
+							<div id="step-counter">
+								<p onClick={this.setStep.bind(this, 1)} className={`${this.state.stepCtr >= 1 ? "active" : ""}`}> 1 </p>
+								<p onClick={this.setStep.bind(this, 2)} className={`${this.state.stepCtr >= 2 ? "active" : ""}`}> 2 </p>
+								<p onClick={this.setStep.bind(this, 3)} className={`${this.state.stepCtr === 3 ? "active" : ""}`}> 3 </p>
+							</div>
 						</div>
-
-						<div id="step-3" className={`step ${this.state.stepCtr === 3 ? "active" : ""}`}>
-							<p> Registration Complete. </p>
-							<p> Please confirm your email. </p>
-						</div>
-					</div>
-
-
-					<div id="signup-controls">
-						<div id="step-nav">
-							<div onClick={this.getPrevStep} className={`arrow-left ${this.state.stepCtr === 2 ? "active" : ""}`}></div>
-							{this.getCurrStep(this.state.stepCtr)}
-							<div onClick={this.getNextStep} className={`arrow-right ${this.state.stepCtr < 3 ? "active" : ""}`}></div>
-						</div>
-
-						<div id="step-counter">
-							<p onClick={this.setStep.bind(this, 1)} className={`${this.state.stepCtr === 1 ? "active" : ""}`}> 1 </p>
-							<p onClick={this.setStep.bind(this, 2)} className={`${this.state.stepCtr === 2 ? "active" : ""}`}> 2 </p>
-							<p onClick={this.setStep.bind(this, 3)} className={`${this.state.stepCtr === 3 ? "active" : ""}`}> 3 </p>
-						</div>
-					</div>
-				</Form>
+					</Form>
 				)}
 			</Formik>
 		)
