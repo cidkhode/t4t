282bd7c57598fea8377fb54f0d07402903330275
cidkhode <chidanandkhode@yahoo.com>
2019-10-10 20:33:54 -0400

    Revert "Registration/Login models and controllers"
    This reverts commit 9e6eac7a14f8405c4a899eca6fa2835f00ea8fd2.

--- src/main/java/com/t4t/thought4thought/springmvc/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-import thought4thought.springmvc.model.Login;
-import thought4thought.springmvc.model.User;
-
-public class UserDao {
-	void register(User user);
-
-	User validateUser(Login login);
-}
