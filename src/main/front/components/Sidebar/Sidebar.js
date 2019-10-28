35529e72ff6496b8c156444f83b3a83dffea6689
Bernin Alejandro Uben <berninuben@icloud.com>
2019-10-28 17:30:20 -0400

    updated sidebar links w/ react-router-dom to reapply/fix active styles

--- src/main/front/components/Sidebar/Sidebar.js
+++ src/main/front/components/Sidebar/Sidebar.js
@@ -10,30 +10,38 @@ export const Sidebar = props => (
     <div className="side-bar-option side-bar-expand" onClick={ props.onOpen }>
       { props.isOpen && <p className="side-bar-option-text">{ props.name }</p> }<span className={ `side-bar-arrow ${props.isOpen ? 'collapse' : 'expand'}-arrow` } />
     </div>
-    <div onClick={ props.isOpen ? () => props.onTopicSelection('myDashboard') : null } className={ `side-bar-option ${props.selectedOption === 'myDashboard' ? 'is-selected' : ''}` }>
-      { props.isOpen && <NavLink exact to="/"><p className="side-bar-option-text">Home</p></NavLink> }
-      <span className="side-bar-option-type" />
-    </div>
-    <div onClick={ props.isOpen ? () => props.onTopicSelection('myDashboard') : null } className={ `side-bar-option ${props.selectedOption === 'myDashboard' ? 'is-selected' : ''}` }>
-      { props.isOpen && <NavLink exact to="/dashboard"><p className="side-bar-option-text">Dashboard</p></NavLink> }
-      <span className="side-bar-option-type" />
-    </div>
-    <div onClick={ props.isOpen ? () => props.onTopicSelection('myAccount') : null } className={ `side-bar-option ${props.selectedOption === 'myAccount' ? 'is-selected' : ''}` }>
-      { props.isOpen && <NavLink exact to="/dashboard/account"><p className="side-bar-option-text">Account</p></NavLink> }
-      <span className="side-bar-option-type" />
-    </div>
+    <NavLink exact to="/" activeClassName="is-selected">
+      <div className="side-bar-option">
+        { props.isOpen && <p className="side-bar-option-text">Home</p> }
+        <span className="side-bar-option-type" />
+      </div>
+    </NavLink>
+    <NavLink exact to="/dashboard" activeClassName="is-selected">
+      <div className="side-bar-option">
+        { props.isOpen && <p className="side-bar-option-text">Dashboard</p> }
+        <span className="side-bar-option-type" />
+      </div>
+    </NavLink>
+    <NavLink exact to="/dashboard/account" activeClassName="is-selected">
+      <div className="side-bar-option">
+        { props.isOpen && <p className="side-bar-option-text">Account</p> }
+        <span className="side-bar-option-type" />
+      </div>
+    </NavLink>
     <div className="side-bar-option topics-sub-menu">
       { props.isOpen && <p className="side-bar-option-text">Topics</p> }<hr />
     </div>
     {
       props.topics.map((topic, key) => (
         <div onClick={ props.isOpen ? () => props.onTopicSelection(topic.key) : null } className={ `side-bar-option ${props.selectedOption === topic.key ? 'is-selected' : ''}` } key={ key }>
-          { props.isOpen && <p className="side-bar-option-text">{topic.title}</p> }<span className="side-bar-option-type" />
+          { props.isOpen && <p className="side-bar-option-text">{topic.title}</p> }
+          <span className="side-bar-option-type" />
         </div>
       ))
     }
     <div onClick={ props.isOpen ? props.onSignOut : null } className="side-bar-option">
-      { props.isOpen && <p className="side-bar-option-text">Sign Out</p> }<span className="side-bar-three-dots">...</span>
+      { props.isOpen && <p className="side-bar-option-text">Sign Out</p> }
+      <span className="side-bar-three-dots">...</span>
     </div>
   </div>
 );
